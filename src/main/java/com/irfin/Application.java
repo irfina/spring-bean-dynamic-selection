9c01f518a1ed51af182b8e778bc7e61cdca98bee
Irfin A <sunwellsystem@gmail.com>
2022-12-09 15:39:19 +0800

    Code cleanup

--- src/main/java/com/irfin/Application.java
+++ src/main/java/com/irfin/Application.java
@@ -5,11 +5,9 @@
  */
 package com.irfin;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 /**
  * @author Irfin
--- src/main/java/com/irfin/adapter/in/web/JobOrderController.java
+++ src/main/java/com/irfin/adapter/in/web/JobOrderController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 /**
  * @author Irfin A.
  */
